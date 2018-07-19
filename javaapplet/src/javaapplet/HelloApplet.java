ce2839bdb4f882f8b0bdc9a6e505d4f264344d47
Jacob J. Hisel <1546024764@mil>
2018-07-19 15:11:50 -0400

    initial commit

---
+++ javaapplet/src/javaapplet/HelloApplet.java
@@ -0,0 +1,11 @@
+package javaapplet;
+
+import java.applet.*;
+import java.awt.*;
+
+public final class HelloApplet extends Applet {
+
+	 public void paint (Graphics g) {
+	      g.drawString ("Hello World", 250, 500);
+	   }
+	}
