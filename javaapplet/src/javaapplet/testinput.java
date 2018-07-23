b291436ab0cdba6f570802d4cc1b15ceceddbeed
Jacob J. Hisel <1546024764@mil>
2018-07-23 12:02:01 -0400

    added form processer and button for form data

---
+++ javaapplet/src/javaapplet/testinput.java
@@ -0,0 +1,30 @@
+package javaapplet;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+import netscape.javascript.*;
+
+
+public class testinput extends JApplet {
+	
+	private JButton button = new JButton("Submit");
+	public void init() {
+		getContentPane().setLayout(new FlowLayout());
+		getContentPane().add(button);
+		button.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent evt) {
+				buttonActionPerformed(evt);
+				}
+			});
+		}
+	public void buttonActionPerformed(ActionEvent evt) throws JSException {
+		try {
+			JSObject jsObj = JSObject.getWindow(this);
+			jsObj.call("submitForm", null);
+			} catch (JSException ex) {
+				ex.printStackTrace();
+				}
+		}
+	}
